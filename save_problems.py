03124e39a930c7286544e1c2cd446ecd72d550a9
Lucas Saldyt <lucassaldyt@gmail.com>
2018-06-01 17:21:47 -0700

    Fixes problem saving

--- save_problems.py
+++ save_problems.py
@@ -1,5 +1,6 @@
 
 def save_problems(df):
     problems = df['problem'].tolist()
+    #print(problems)
     with open('problems.txt', 'w') as outfile:
-        outfile.write('\n'.join(problems))
+        outfile.write('\n'.join(' '.join(map(str, p)) for p in problems))
